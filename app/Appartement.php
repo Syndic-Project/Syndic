32bfd92e60d76d8e400e8ac86e677f3871bd199a
Hicham <tony.theedev@gmail.com>
2020-10-12 17:25:14 +0100

    model appartement (not ready)

--- app/Appartement.php
+++ app/Appartement.php
@@ -4,7 +4,25 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id_appartement
+ * @property string $type_de_bien
+ * @property int $nbr_max
+ * @property string $nom_appartement
+ * @property int $num_appartement
+ */
 class Appartement extends Model
 {
-    //
+    /**
+     * The primary key for the model.
+     * 
+     * @var string
+     */
+    protected $primaryKey = 'id_appartement';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['type_de_bien', 'nbr_max', 'nom_appartement', 'num_appartement'];
+
 }
